c864f8911eb46cd3386cbcd357b250b38a567c78
ramprsd1202 <ramprsd1202@gmail.com>
2016-04-08 19:48:56 +0530

    commit 1

--- learnmaven/src/main/java/myclass/dev/MyFirst.java
+++ learnmaven/src/main/java/myclass/dev/MyFirst.java
@@ -1,5 +1,6 @@
 package myclass.dev;
 
 public class MyFirst {
-
+private int a;
+	
 }
--- learnmaven/src/test/java/myclass/dev/MyFirstTest.java
+++ learnmaven/src/test/java/myclass/dev/MyFirstTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 public class MyFirstTest {
 	@Test
 	public boolean mytest(){
+		System.out.println("dfdsf");
 		
 		return true;
 	}
